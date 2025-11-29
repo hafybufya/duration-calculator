ebccc39175369c18e64efd2ee0e8e0bfa97101d7
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-11-29 16:58:05 +0000

    modification: added unit tests for datet_time_calc

--- unitTest.py
+++ unitTest.py
@@ -1,15 +1,43 @@
 import unittest
 from  mainCode import date_time_calc
-import numpy as np
+from mainCode import *
+from unittest.mock import Mock, MagicMock, patch
+import pytest
+
+
 # define the unit tests
 class my_unit_tests(unittest.TestCase):
 
-    def test_date_difference(self):
-        """Test that date difference is calculated correctly."""
-        actual_result = date_time_calc("2025-10-10")
-        expected_result = np.datetime64('today') - np.datetime64("2025-10-10")
-        self.assertEqual(actual_result, expected_result)
 
+# ---------------------------------------------------------------------
+# TESTING: date_time_calc()
+# ---------------------------------------------------------------------
+
+    # Tests to see if function takes correct values and calculates correctly
+    # in format YYYY-MM-DD
+    def test_valid_date_ymd(self):
+        with patch("builtins.input", return_value="2025-10-10"):
+            result = date_time_calc()
+            assert result == 50
+
+    # Tests to see if function takes correct values and calculates correctly
+    # in format YYYY-MM
+    def test_valid_date_ym(self):
+        with patch("builtins.input", return_value="2025-10"):
+            result = date_time_calc()
+            assert result == 59
+
+    # Tests to see if users can pass in strings
+    def test_string_date(self):
+        with patch("builtins.input", return_value="abc"):
+            with pytest.raises(ValueError, match = error_message):
+                date_time_calc()
+
+    # Tests to see if users can pass in numbers
+    def test_number_date(self):
+        with patch("builtins.input", return_value="2050"):
+            with pytest.raises(ValueError, match = error_message):
+                date_time_calc()
 
 
     # run the tests
